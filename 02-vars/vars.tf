23e72a0d15c4b5f8b470b2fd7e9b72331f5c7838
Samdevops789 <Sampathbandla2@gmail.com>
2022-11-24 16:09:43 +0530

    updating ./learning-devops7/learning-terraform

--- 02-vars/vars.tf
+++ 02-vars/vars.tf
@@ -6,20 +6,20 @@ output "test" {
   value = var.test
 }
 
-variable "test1" {}
+variable "test1" { }
 
 output "test1" {
   value = var.test1
 }
 
-variable "test2" {}
+variable "test2" { }
 
 output "test2" {
   value = var.test2
 }
 
-variable "test3" {}
+variable "test3" { }
 
 output "test3" {
   value = var.test3
-  }
+}
