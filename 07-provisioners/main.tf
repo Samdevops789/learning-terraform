4b572975236a84e9a83132e23baae3fec1f7a084
Samdevops789 <Sampathbandla2@gmail.com>
2023-01-08 20:46:32 +0530

    updating ./learning-devops7/learning-terraform

--- 07-provisioners/main.tf
+++ 07-provisioners/main.tf
@@ -1,18 +1,43 @@
+//resource "aws_instance" "web" {
+//  ami                    = "ami-02e5183eb59de7943"
+//  instance_type          = "t3.micro"
+//  vpc_security_group_ids = ["sg-07f79705c0e4d936b"]
+//
+//  provisioner "remote-exec" {
+//
+//    connection {
+//      host     = self.public_ip
+//      user     = "root"
+//      password = "DevOps321"
+//    }
+//
+//   inline = [
+//      "echo Hello World"
+//    ]
+//
+//  }
+//}
+
 resource "aws_instance" "web" {
   ami                    = "ami-02e5183eb59de7943"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-07f79705c0e4d936b"]
+}
 
+resource "null_resource" "cli" {
+  triggers = {
+    abc = timestamp()
+  }
   provisioner "remote-exec" {
 
     connection {
-      host     = self.public_ip
+      host     = aws_instance.web.public_ip
       user     = "root"
       password = "DevOps321"
     }
 
     inline = [
-      "echo Hello World"
+      "echo Hello Galaxy"
     ]
 
   }
