4da046e956642b586a590effea00b0ff615d1c81
Samdevops789 <Sampathbandla2@gmail.com>
2023-01-08 18:46:06 +0530

    updating ./learning-devops7/learning-terraform

---
+++ 05-loops/for-each/main.tf
@@ -0,0 +1,12 @@
+//resource "aws_instance" "web" {
+//  for_each      = toset(var.sample)
+//  ami           = "ami-07ef4d849950da0b3"
+//  instance_type = "t3.micro"
+//  tags = {
+//    Name = each.key
+//  }
+//}
+//
+//variable "sample" {
+//default = ["one", "two"]
+//}
